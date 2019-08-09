a3cc6c37c366a52df83bd23b3cef4899be5d05cd
David Sandoval <dmagia7@users.noreply.github.com>
2019-08-09 17:57:30 -0500

    mongo db

--- src/database.js
+++ src/database.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/mern-tasks';
+const URI = 'mongodb+srv://<admin>:<YnLEYDS4cA457z0m>@mern-task-fhxxi.mongodb.net/test?retryWrites=true&w=majority';
 
 
 mongoose.connect(URI)
+
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
 

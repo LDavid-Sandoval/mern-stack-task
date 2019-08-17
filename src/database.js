1fe568ce7bf36b13d9cb916052e74a546be23a53
David Sandoval <dmagia7@users.noreply.github.com>
2019-08-17 01:06:15 -0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb+srv://admin:YnLEYDS4cA457z0m@mern-task-fhxxi.mongodb.net/test?retryWrites=true&w=majority';
+const URI = 'mongodb+srv://admin:Admin@mern-task-fhxxi.mongodb.net/test?retryWrites=true&w=majority';
 
 
 mongoose.connect(URI)
-
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
-
 module.exports = mongoose;
